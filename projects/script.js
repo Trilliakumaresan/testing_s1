49c86dda91dd7b41a8d52ef4b16e096ae8c37684
Trilliakumaresan <158392382+Trilliakumaresan@users.noreply.github.com>
2025-09-04 17:00:10 -0700

    script.js

--- projects/script.js
+++ projects/script.js
@@ -1,3 +1,44 @@
+$(document).ready(function () {
+
+    $('#menu').click(function () {
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('scroll load', function () {
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if (window.scrollY > 60) {
+            document.querySelector('#scroll-top').classList.add('active');
+        } else {
+            document.querySelector('#scroll-top').classList.remove('active');
+        }
+    });
+});
+
+document.addEventListener('visibilitychange',
+    function () {
+        if (document.visibilityState === "visible") {
+            document.title = "Projects | Portfolio Trillia Kumaresan";
+            $("#favicon").attr("href", "/assets/images/favicon.png");
+        }
+        else {
+            document.title = "Come Back To Portfolio";
+            $("#favicon").attr("href", "/assets/images/favhand.png");
+        }
+    });
+
+
+// fetch projects start
+function getProjects() {
+    return fetch("projects.json")
+        .then(response => response.json())
+        .then(data => {
+            return data
+        });
+}
+
 
 function showProjects(projects) {
     let projectsContainer = document.querySelector(".work .box-container");
@@ -5,16 +46,16 @@ function showProjects(projects) {
     projects.forEach(project => {
         projectsHTML += `
         <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="./assets/images/educat/college.jpg" alt="" />
+      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
-        <h3>NotyStack Android</h3>
+        <h3>${project.name}</h3>
         </div>
         <div class="desc">
-          <p>Fully fledged android CRUD app based on different design patterns. Built using Java, XML, SQLite, Firebase, RoomDB, MVVM.</p>
+          <p>${project.desc}</p>
           <div class="btns">
-            <a href="https://www.linkedin.com/posts/jigar-sable_androiddev-androidstudio-java-activity-6955224473822199808-JT0j" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="https://github.com/jigar-sable/notystack-android" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
+            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
           </div>
         </div>
       </div>
